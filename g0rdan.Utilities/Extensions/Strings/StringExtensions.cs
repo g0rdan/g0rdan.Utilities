d8c2de80aee5257cb267d74894b7803c5df2d94f
Denis Gordin <gordin.dan@gmail.com>
2015-11-16 15:11:44 +0300

    init commit

---
+++ g0rdan.Utilities/Extensions/Strings/StringExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text;
+
+namespace g0rdan.Utilities
+{
+    public static class StringExtensions
+    {
+        /// <summary>
+        /// Replace html tags to string equivalents
+        /// </summary>
+        /// <returns>The html to string.</returns>
+        /// <param name="value">Value.</param>
+        public static string ClearHTML(this string value)
+        {
+            if (value.ToLower().Contains("<br>"))
+            {
+                value = value.Replace("<br>", "\n");
+            }
+
+            return value;
+        }
+    }
+}
+
