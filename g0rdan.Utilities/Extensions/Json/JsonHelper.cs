6a057595f430f9c396d1345713ae50042ae38dd5
Denis Gordin <gordin.dan@gmail.com>
2015-11-17 23:09:07 +0300

    add json extension methods

---
+++ g0rdan.Utilities/Extensions/Json/JsonHelper.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+
+namespace g0rdan.Utilities
+{
+    public static class JsonHelper
+    {
+        public static string Serialize(this object obj)
+        {
+            return JsonConvert.SerializeObject(obj);
+        }
+
+        public static T Deserialize<T>(this string model)
+        {
+            try
+            {
+                return JsonConvert.DeserializeObject<T>(model);
+            }
+            catch
+            {
+                return default(T);
+            }
+        }
+    }
+}
