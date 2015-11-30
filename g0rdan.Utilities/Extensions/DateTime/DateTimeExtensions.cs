36ffe1ed36acedf96943fd148d72893cffa75040
Denis Gordin <gordin.dan@gmail.com>
2015-11-30 18:42:55 +0300

    добавил date time вспомогательные методы

---
+++ g0rdan.Utilities/Extensions/DateTime/DateTimeExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace g0rdan.Utilities
+{
+    public static class DateTimeExtensions
+    {
+        /// <summary>
+        /// Перевод времени в unix штамп
+        /// </summary>
+        /// <returns>The unix time.</returns>
+        /// <param name="dateTime">Date time.</param>
+        public static double DateTimeToUnixTime(this DateTime dateTime)
+        {
+            return (DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1))).TotalSeconds;
+        }
+
+        /// <summary>
+        /// Перевод unix штампа в DateTime
+        /// </summary>
+        /// <returns>The time to date time.</returns>
+        /// <param name="unixTimeStamp">Unix time stamp.</param>
+        public static DateTime UnixTimeToDateTime(this double unixTimeStamp)
+        {
+            var dtDateTime = new DateTime(1970,1,1,0,0,0,0,System.DateTimeKind.Utc);
+            dtDateTime = dtDateTime.AddSeconds( unixTimeStamp ).ToLocalTime();
+            return dtDateTime;
+        }
+    }
+}
+
